0d9e7f2e1887974f25092e3a4dc27281474226bb
Victor <vccabral@gmail.com>
2012-12-10 10:22:35 -0500

    Update startDjango.sh

--- startDjango.sh
+++ startDjango.sh
@@ -1,15 +1,17 @@
-#/bin/bash
+#!/bin/bash
 EXPECTED_ARGS=1
 E_BADARGS=65
 
 if [ $# -eq $EXPECTED_ARGS ]
 then
     django-admin.py startproject --template=https://github.com/vccabral/DjangoTemplate/zipball/master $1
-    virtualenv localpython
-    source localpython/bin/activate
-    cd $1
+    echo $1
+    cd ./$1
+    virtualenv venv --distribute
+    source venv/bin/activate
+    echo "install requirements"
     pip install -r requirements.txt
+    echo "done installing"
 else
     echo "First argument must be the name of the new project"
 fi 
-
